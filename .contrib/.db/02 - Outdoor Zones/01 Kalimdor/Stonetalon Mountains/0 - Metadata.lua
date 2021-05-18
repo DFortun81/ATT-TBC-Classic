4b1ea96afb714bcd7062b78f4ebc593a7bf4bd6a
Crieve <dylanfortune@live.com>
2021-05-18 16:56:33 -0700

    With the migration of Classic into Burning Crusade Classic, the database for both versions of the addon has been moved over to the TBC repository. Regular content patches/updates will still be made to the Classic Era version of ATT-Classic.

---
+++ .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Stonetalon Mountains/0 - Metadata.lua
@@ -0,0 +1,11 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(KALIMDOR, {
+		m(STONETALON_MOUNTAINS, {
+			["description"] = "The Stonetalon Mountains are craggy and forbidding. The air is usually hot, though violent rainstorms and severe winds cool the slopes and endanger travelers. The mountains are home to many fi erce creatures, including hippogryphs, harpies, drakes, wyverns and kobolds. This region is inhospitable and extreme.",
+		}),
+	}),
+};
---
+++ .contrib/.db/02 - Outdoor Zones/02 Eastern Kingdoms/Blackrock Mountain/Zone Drops.lua
@@ -0,0 +1,16 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+_.Zones =
+{
+	m(EASTERN_KINGDOMS, {
+		m(BLACKROCK_MOUNTAIN, {
+			n(ZONEDROPS, {
+				i(18987, {	-- Blackhand's Command
+					["cr"] = 9046,	-- Scarshield Quartermaster <Scarshield Legion>
+					["questID"] = 7761,	-- Blackhand's Command
+				}),
+			}),
+		}),
+	}),
+};
