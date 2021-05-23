5a65fd22f55da0d21d2688494cb9d4b0d1b3361b
Crieve <dylanfortune@live.com>
2021-05-22 21:47:11 -0700

    Moved the Jewelcrafting recipe definitions from Kara and Hyjal into the Jewelcrafting Professions file.

---
+++ .contrib/.db/03 - Professions/Jewelcrafting/Jewelcrafting.lua
@@ -0,0 +1,429 @@
+applyclassicphase(TBC_PHASE_ONE, profession(JEWELCRAFTING, {
+	tier(1, {	-- Classic
+		filter(52, {	-- Finger
+			{
+				["name"] = "Aquamarine Signet",
+				["recipeID"] = 26874
+			},
+			{
+				["name"] = "Blazing Citrine Ring",
+				["recipeID"] = 25617
+			},
+			{
+				["name"] = "Braided Copper Ring",
+				["recipeID"] = 25493
+			},
+			{
+				["name"] = "Bronze Band of Force",
+				["recipeID"] = 37818
+			},
+			{
+				["name"] = "Citrine Ring of Rapid Healing",
+				["recipeID"] = 25621
+			},
+			{
+				["name"] = "Diamond Focus Ring",
+				["recipeID"] = 36526
+			},
+			{
+				["name"] = "Elegant Silver Ring",
+				["recipeID"] = 25280
+			},
+			{
+				["name"] = "Emerald Lion Ring",
+				["recipeID"] = 34961
+			},
+			{
+				["name"] = "Engraved Truesilver Ring",
+				["recipeID"] = 25620
+			},
+			{
+				["name"] = "Gem Studded Band",
+				["recipeID"] = 26896
+			},
+			{
+				["name"] = "Gloom Band",
+				["recipeID"] = 25287
+			},
+			{
+				["name"] = "Glowing Thorium Band",
+				["recipeID"] = 34960
+			},
+			{
+				["name"] = "Golden Dragon Ring",
+				["recipeID"] = 25613
+			},
+			{
+				["name"] = "Golden Ring of Power",
+				["recipeID"] = 34955
+			},
+			{
+				["name"] = "Heavy Copper Ring",
+				["recipeID"] = 26926
+			},
+			{
+				["name"] = "Heavy Jade Ring",
+				["recipeID"] = 36524
+			},
+			{
+				["name"] = "Heavy Silver Ring",
+				["recipeID"] = 25305
+			},
+			{
+				["name"] = "Inlaid Malachite Ring",
+				["recipeID"] = 25283
+			},
+			{
+				["name"] = "Onslaught Ring",
+				["recipeID"] = 26907
+			},
+			{
+				["name"] = "Red Ring of Destruction",
+				["recipeID"] = 36525
+			},
+			{
+				["name"] = "Ring of Bitter Shadows",
+				["recipeID"] = 26910
+			},
+			{
+				["name"] = "Ring of Silver Might",
+				["recipeID"] = 25317
+			},
+			{
+				["name"] = "Ring of Twilight Shadows",
+				["recipeID"] = 25318
+			},
+			{
+				["name"] = "Sapphire Signet",
+				["recipeID"] = 26903
+			},
+			{
+				["name"] = "Simple Opal Ring",
+				["recipeID"] = 26902
+			},
+			{
+				["name"] = "Simple Pearl Ring",
+				["recipeID"] = 25284
+			},
+			{
+				["name"] = "Solid Bronze Ring",
+				["recipeID"] = 25490
+			},
+			{
+				["name"] = "The Aquamarine Ward",
+				["recipeID"] = 26887
+			},
+			{
+				["name"] = "The Jade Eye",
+				["recipeID"] = 25619
+			},
+			{
+				["name"] = "Tigerseye Band",
+				["recipeID"] = 32179
+			},
+			{
+				["name"] = "Truesilver Commander's Ring",
+				["recipeID"] = 34959
+			},
+			{
+				["name"] = "Truesilver Healing Ring",
+				["recipeID"] = 26885
+			},
+			{
+				["name"] = "Wicked Moonstone Ring",
+				["recipeID"] = 25323
+			},
+			{
+				["name"] = "Woven Copper Ring",
+				["recipeID"] = 26925
+			}
+		}),
+		filter(51, {	-- Neck
+			{
+				["name"] = "Amulet of the Moon",
+				["recipeID"] = 25339
+			},
+			{
+				["name"] = "Amulet of Truesight",
+				["recipeID"] = 63743
+			},
+			{
+				["name"] = "Aquamarine Pendant of the Warrior",
+				["recipeID"] = 26876
+			},
+			{
+				["name"] = "Barbaric Iron Collar",
+				["recipeID"] = 25498
+			},
+			{
+				["name"] = "Brilliant Necklace",
+				["recipeID"] = 36523
+			},
+			{
+				["name"] = "Bronze Torc",
+				["recipeID"] = 38175
+			},
+			{
+				["name"] = "Citrine Pendant of Golden Healing",
+				["recipeID"] = 25622
+			},
+			{
+				["name"] = "Heavy Golden Necklace of Battle",
+				["recipeID"] = 25320
+			},
+			{
+				["name"] = "Jade Pendant of Blasting",
+				["recipeID"] = 25618
+			},
+			{
+				["name"] = "Living Emerald Pendant",
+				["recipeID"] = 26911
+			},
+			{
+				["name"] = "Malachite Pendant",
+				["recipeID"] = 32178
+			},
+			{
+				["name"] = "Necklace of the Diamond Tower",
+				["recipeID"] = 26915
+			},
+			{
+				["name"] = "Opal Necklace of Impact",
+				["recipeID"] = 26897
+			},
+			{
+				["name"] = "Ornate Tigerseye Necklace",
+				["recipeID"] = 26928
+			},
+			{
+				["name"] = "Pendant of the Agate Shield",
+				["recipeID"] = 25610
+			},
+			{
+				["name"] = "Ruby Pendant of Fire",
+				["recipeID"] = 26883
+			},
+			{
+				["name"] = "Sapphire Pendant of Winter Night",
+				["recipeID"] = 26908
+			},
+			{
+				["name"] = "Silver Rose Pendant",
+				["recipeID"] = 25614
+			},
+			{
+				["name"] = "Thick Bronze Necklace",
+				["recipeID"] = 26927
+			}
+		}),
+		category(20, {	-- Materials
+			{
+				["name"] = "Bronze Setting",
+				["recipeID"] = 25278
+			},
+			{
+				["name"] = "Delicate Copper Wire",
+				["recipeID"] = 25255
+			},
+			{
+				["name"] = "Mithril Filigree",
+				["recipeID"] = 25615
+			},
+			{
+				["name"] = "Thorium Setting",
+				["recipeID"] = 26880
+			}
+		}),
+		category(23, {	-- Helms
+			{
+				["name"] = "Emerald Crown of Destruction",
+				["recipeID"] = 26906
+			},
+			{
+				["name"] = "Moonsoul Crown",
+				["recipeID"] = 25321
+			},
+			{
+				["name"] = "Ruby Crown of Restoration",
+				["recipeID"] = 26878
+			}
+		}),
+		filter(53, {	-- Trinkets
+			{
+				["name"] = "Figurine - Black Diamond Crab",
+				["recipeID"] = 26912
+			},
+			{
+				["name"] = "Figurine - Black Pearl Panther",
+				["recipeID"] = 26875
+			},
+			{
+				["name"] = "Figurine - Dark Iron Scorpid",
+				["recipeID"] = 26914
+			},
+			{
+				["name"] = "Figurine - Emerald Owl",
+				["recipeID"] = 26909
+			},
+			{
+				["name"] = "Figurine - Golden Hare",
+				["recipeID"] = 26873
+			},
+			{
+				["name"] = "Figurine - Jade Owl",
+				["recipeID"] = 26872
+			},
+			{
+				["name"] = "Figurine - Ruby Serpent",
+				["recipeID"] = 26900
+			},
+			{
+				["name"] = "Figurine - Truesilver Boar",
+				["recipeID"] = 26882
+			},
+			{
+				["name"] = "Figurine - Truesilver Crab",
+				["recipeID"] = 26881
+			}
+		}),
+		filter(34, {	-- Fist Weapons
+			{
+				["name"] = "Heavy Iron Knuckles",
+				["recipeID"] = 25612
+			}
+		}),
+		filter(50, {	-- Miscellaneous
+			{
+				["name"] = "Coarse Stone Statue",
+				["recipeID"] = 32801
+			},
+			{
+				["name"] = "Dense Stone Statue",
+				["recipeID"] = 32809
+			},
+			{
+				["name"] = "Heavy Stone Statue",
+				["recipeID"] = 32807
+			},
+			{
+				["name"] = "Rough Stone Statue",
+				["recipeID"] = 32259
+			},
+			{
+				["name"] = "Solid Stone Statue",
+				["recipeID"] = 32808
+			}
+		}),
+	}),
+	tier(2, {	-- Burning Crusade
+		
+	}),
+	applyclassicphase(WRATH_PHASE_ONE, tier(3, {	-- Wrath of the Lich King
+		
+	})),
+	applyclassicphase(CATA_PHASE_ONE, tier(4, {	-- Cataclysm
+		
+	})),
+	applyclassicphase(MOP_PHASE_ONE, tier(5, {	-- Mists of Pandaria
+		
+	})),
+	applyclassicphase(WOD_PHASE_ONE, tier(6, {	-- Warlords of Draenor
+		
+	})),
+	applyclassicphase(LEGION_PHASE_ONE, tier(7, {	-- Legion
+		
+	})),
+	applyclassicphase(BFA_PHASE_ONE, tier(8, {	-- BFA
+		
+	})),
+	applyclassicphase(SHADOWLANDS_PHASE_ONE, tier(8, {	-- Shadowlands
+		
+	})),
+}));
+
+-- Jewelcrafting Item Database
+_.ItemDB = {};
+local REMOVED_WITH_CATA = "removed 4.0.1.10000";
+
+-- Jewelcrafting Item Recipe Database
+local itemrecipe = function(name, itemID, spellID, spellIDAfterCata, timeline)
+	local o = { ["itemID"] = itemID, ["spellID"] = spellID, ["requireSkill"] = JEWELCRAFTING, ["f"] = 200 };
+	if spellIDAfterCata then
+		if type(spellIDAfterCata) ~= "number" then
+			-- #if AFTER CATA
+			o.spellID = spellIDAfterCata;
+			-- #endif
+		else
+			-- Check to see if the timeline was shifted over one argument.
+			-- A non-numerical argument for the spellID is not valid, so it must have been intended.
+			if not timeline then timeline = spellIDAfterCata; end
+		end
+	end
+	if timeline then
+		-- Ensure that the timeline is in a table format.
+		if type(timeline) == "string" then timeline = { timeline }; end
+		o.timeline = timeline;
+	end
+	if name then
+		-- Ensure that the name is in a string format.
+		if type(name) == "table" then
+			-- #if AFTER CATA
+			name = name[2];
+			-- #else
+			name = name[1];
+			-- #endif
+		end
+		o.name = name;
+	end
+	_.ItemDB[itemID] = applyclassicphase(TBC_PHASE_ONE, o);
+	return o;
+end
+
+-- #if AFTER TBC
+-- Recipes introduced with Burning Crusade
+-- itemrecipe("", ITEMID, SPELLID);
+itemrecipe("Design: Bracing Earthstorm Diamond", 25903, 32867);
+itemrecipe("Design: Powerful Earthstorm Diamond", 25902, 32866);
+itemrecipe("Design: Bold Crimson Spinel", 32274, 39705);
+itemrecipe("Design: Delicate Crimson Spinel", 32277, 39706);
+itemrecipe("Design: Forceful Seaspray Emerald", 35765, 47053);
+itemrecipe("Design: Jagged Seaspray Emerald", 32312, 39742);
+itemrecipe("Design: Potent Pyrestone", 32304, 39734);
+itemrecipe("Design: Quick Lionseye", 35763, 47056);
+itemrecipe("Design: Radiant Seaspray Emerald", 32310, 39740);
+itemrecipe("Design: Reckless Pyrestone", 35762, 47055);
+itemrecipe("Design: Smooth Lionseye", 32291, 39720);
+itemrecipe("Design: Solid Empyrean Sapphire", 32286, 39715);
+itemrecipe("Design: Sparkling Empyrean Sapphire", 32287, 39716);
+itemrecipe("Design: Steady Seaspray Emerald", 35764, 47054);
+itemrecipe("Design: Subtle Lionseye", 32284, 39713);
+
+-- Recipes Renamed with Cata
+-- itemrecipe({" [TBC]", " [CATA+]"}, ITEMID, SPELLID);
+itemrecipe({"Design: Dazzling Seaspray Emerald [TBC]", "Design: Purified Shadowsong Amethyst [CATA+]"}, 32311, 39741);
+itemrecipe({"Design: Enduring Seaspray Emerald [TBC]", "Design: Regal Seaspray Emerald [CATA+]"}, 32309, 39739);
+itemrecipe({"Design: Glinting Pyrestone [TBC]", "Design: Glinting Shadowsong Amethyst [CATA+]"}, 32306, 39736);
+itemrecipe({"Design: Rigid Lionseye [TBC]", "Design: Rigid Empyrean Sapphire [CATA+]"}, 32292, 39721);
+itemrecipe({"Design: Runed Crimson Spinel [TBC]", "Design: Brilliant Crimson Spinel [CATA+]"}, 32282, 39711);
+itemrecipe({"Design: Wicked Pyrestone [TBC]", "Design: Deadly Pyrestone [CATA+]"}, 32308, 39738);
+
+-- Recipes Removed with Cata
+-- itemrecipe({" [TBC]", " [CATA+]"}, ITEMID, SPELLID, AFTERCATA, REMOVED_WITH_CATA);
+itemrecipe({"Design: Balanced Shadowsong Amethyst [TBC]", "Design: Shifting Shadowsong Amethyst [CATA+]"}, 32299, 39729, 39728, REMOVED_WITH_CATA);
+itemrecipe({"Design: Bright Crimson Spinel [TBC]", "Design: Delicate Crimson Spinel [CATA+]"}, 32283, 39712, 39706, REMOVED_WITH_CATA);
+itemrecipe({"Design: Brilliant Lionseye [TBC]", "Design: Brilliant Crimson Spinel [CATA+]"}, 32290, 39719, 39711, REMOVED_WITH_CATA);
+itemrecipe({"Design: Gleaming Lionseye [TBC]", "Design: Smooth Lionseye [CATA+]"}, 32293, 39722, 39720, REMOVED_WITH_CATA);
+itemrecipe({"Design: Glowing Shadowsong Amethyst [TBC]", "Design: Timeless Shadowsong Amethyst [CATA+]"}, 32301, 39731, 39731, REMOVED_WITH_CATA);
+itemrecipe({"Design: Infused Shadowsong Amethyst [TBC]", "Design: Glinting Shadowsong Amethyst [CATA+]"}, 32300, 39730, 39736, REMOVED_WITH_CATA);
+itemrecipe({"Design: Luminous Pyrestone [TBC]", "Design: Reckless Pyrestone [CATA+]"}, 32305, 39735, 47055, REMOVED_WITH_CATA);
+itemrecipe({"Design: Lustrous Empyrean Sapphire [TBC]", "Design: Sparkling Empyrean Sapphire [CATA+]"}, 32288, 39717, 39716, REMOVED_WITH_CATA);
+itemrecipe({"Design: Royal Shadowsong Amethyst [TBC]", "Design: Purified Shadowsong Amethyst [CATA+]"}, 32302, 39732, 39741, REMOVED_WITH_CATA);
+itemrecipe({"Design: Teardrop Crimson Spinel [TBC]", "Design: Brilliant Crimson Spinel [CATA+]"}, 32281, 39710, 39711, REMOVED_WITH_CATA);
+itemrecipe({"Design: Thick Lionseye [TBC]", "Design: Subtle Lionseye [CATA+]"}, 32294, 39723, 39713, REMOVED_WITH_CATA);
+-- #endif
+
+-- #if AFTER SHADOWLANDS
+
+-- #endif
+
+
