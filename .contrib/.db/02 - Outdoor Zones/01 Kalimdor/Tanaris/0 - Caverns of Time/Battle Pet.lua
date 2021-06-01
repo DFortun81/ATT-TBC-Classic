a696e4a52748bc0093b79f15e8456304535c4e80
Crieve <dylanfortune@live.com>
2021-06-01 03:34:08 -0700

    Added the Caverns of Time vendor.

---
+++ .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Tanaris/0 - Caverns of Time/Battle Pet.lua
@@ -0,0 +1,20 @@
+---------------------------------------------------
+--          Z O N E S        M O D U L E         --
+---------------------------------------------------
+-- #if AFTER MOP
+_.Zones =
+{
+	m(KALIMDOR, {
+		m(TANARIS, {
+			m(CAVERNS_OF_TIME, {
+				filter(101, {	-- Pet Battle
+					p(1161, {	-- Infinite Whelpling
+						["crs"] = { 68820 },	-- Infinite Whelpling
+						["description"] = "This pet can be found around the Caverns of Time entrance and the pathway leading to the main chamber.",
+					}),
+				}),
+			}),
+		}),
+	}),
+};
+-- #endif
