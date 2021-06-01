6fb51a405f0ee755bf2e1ffff9fe17bba915ea91
Crieve <dylanfortune@live.com>
2021-05-31 22:44:33 -0700

    ... told ya.

--- .contrib/.db/03 - Professions/Engineering/Engineering.lua
+++ .contrib/.db/03 - Professions/Engineering/Engineering.lua
@@ -99,10 +99,10 @@ profession(ENGINEERING, {
 					["name"] = "Lifelike Mechanical Toad",
 					["recipeID"] = 19793
 				},
-				{
+				applyclassicphase(PHASE_ONE_DIREMAUL, {
 					["name"] = "Major Recombobulator",
 					["recipeID"] = 23079,
-				},
+				}),
 				{
 					["name"] = "Masterwork Target Dummy",
 					["recipeID"] = 19814
@@ -506,6 +506,10 @@ profession(ENGINEERING, {
 					["name"] = "Dense Blasting Powder",
 					["recipeID"] = 19788
 				},
+				applyclassicphase(TBC_PHASE_ONE, {
+					["name"] = "Fused Wiring",
+					["recipeID"] = 39895
+				}),
 				{
 					["name"] = "Gold Power Core",
 					["recipeID"] = 12584
@@ -614,7 +618,359 @@ profession(ENGINEERING, {
 		},
 	}),
 	applyclassicphase(TBC_PHASE_ONE, tier(2, {	-- Burning Crusade
-		
+		{
+			["name"] = "Reagents",
+			["categoryID"] = 750,
+			["g"] = {
+				{
+					["name"] = "Adamantite Frame",
+					["recipeID"] = 30306
+				},
+				{
+					["name"] = "Elemental Blasting Powder",
+					["recipeID"] = 30303
+				},
+				{
+					["name"] = "Fel Iron Casing",
+					["recipeID"] = 30304
+				},
+				{
+					["name"] = "Felsteel Stabilizer",
+					["recipeID"] = 30309
+				},
+				{
+					["name"] = "Handful of Fel Iron Bolts",
+					["recipeID"] = 30305
+				},
+				{
+					["name"] = "Hardened Adamantite Tube",
+					["recipeID"] = 30307
+				},
+				{
+					["name"] = "Icy Blasting Primers",
+					["recipeID"] = 39971
+				},
+				{
+					["name"] = "Khorium Power Core",
+					["recipeID"] = 30308
+				}
+			}
+		},
+		{
+			["name"] = "Explosives",
+			["categoryID"] = 751,
+			["g"] = {
+				{
+					["name"] = "Adamantite Grenade",
+					["recipeID"] = 30311
+				},
+				{
+					["name"] = "Elemental Seaforium Charge",
+					["recipeID"] = 30547
+				},
+				{
+					["name"] = "Fel Iron Bomb",
+					["recipeID"] = 30310
+				},
+				{
+					["name"] = "Frost Grenade",
+					["recipeID"] = 39973
+				},
+				{
+					["name"] = "Super Sapper Charge",
+					["recipeID"] = 30560
+				},
+				{
+					["name"] = "The Bigger One",
+					["recipeID"] = 30558
+				}
+			}
+		},
+		{
+			["name"] = "Goggles",
+			["categoryID"] = 752,
+			["g"] = {
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Annihilator Holo-Gogs",
+					["classes"] = { PRIEST, MAGE, WARLOCK },
+					["recipeID"] = 46111
+				}),
+				{
+					["name"] = "Cogspinner Goggles",
+					["recipeID"] = 30316
+				},
+				{
+					["name"] = "Deathblow X11 Goggles",
+					["recipeID"] = 41317
+				},
+				{
+					["name"] = "Destruction Holo-gogs",
+					["recipeID"] = 41320
+				},
+				{
+					["name"] = "Furious Gizmatic Goggles",
+					["recipeID"] = 40274
+				},
+				{
+					["name"] = "Gadgetstorm Goggles",
+					["recipeID"] = 41315
+				},
+				{
+					["name"] = "Gnomish Battle Goggles",
+					["recipeID"] = 30575
+				},
+				{
+					["name"] = "Gnomish Power Goggles",
+					["recipeID"] = 30574
+				},
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Hard Khorium Goggles",
+					["classes"] = { WARRIOR, PALADIN, DEATHKNIGHT },
+					["recipeID"] = 46115
+				}),
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Hyper-Magnified Moon Specs",
+					["classes"] = { DRUID },
+					["recipeID"] = 46109
+				}),
+				{
+					["name"] = "Hyper-Vision Goggles",
+					["recipeID"] = 30325
+				},
+				{
+					["name"] = "Justicebringer 2000 Specs",
+					["recipeID"] = 41311
+				},
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Justicebringer 3000 Specs",
+					["classes"] = { PALADIN },
+					["recipeID"] = 46107
+				}),
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Lightning Etched Specs",
+					["classes"] = { SHAMAN },
+					["recipeID"] = 46112
+				}),
+				{
+					["name"] = "Living Replicator Specs",
+					["recipeID"] = 41316
+				},
+				{
+					["name"] = "Magnified Moon Specs",
+					["recipeID"] = 41319
+				},
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Mayhem Projection Goggles",
+					["classes"] = { WARRIOR, PALADIN, DEATHKNIGHT },
+					["recipeID"] = 46114
+				}),
+				{
+					["name"] = "Power Amplification Goggles",
+					["recipeID"] = 30317
+				},
+				{
+					["name"] = "Powerheal 4000 Lens",
+					["recipeID"] = 41321
+				},
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Powerheal 9000 Lens",
+					["classes"] = { PRIEST },
+					["recipeID"] = 46108
+				}),
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Primal-Attuned Goggles",
+					["classes"] = { SHAMAN },
+					["recipeID"] = 46110
+				}),
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Quad Deathblow X44 Goggles",
+					["classes"] = { ROGUE, MONK, DRUID },
+					["recipeID"] = 46116
+				}),
+				{
+					["name"] = "Surestrike Goggles v2.0",
+					["recipeID"] = 41314
+				},
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Surestrike Goggles v3.0",
+					["classes"] = { HUNTER, SHAMAN },
+					["recipeID"] = 46113
+				}),
+				{
+					["name"] = "Tankatronic Goggles",
+					["recipeID"] = 41312
+				},
+				{
+					["name"] = "Ultra-Spectropic Detection Goggles",
+					["recipeID"] = 30318
+				},
+				{
+					["name"] = "Wonderheal XT40 Shades",
+					["recipeID"] = 41318
+				},
+				applyclassicphase(TBC_PHASE_FIVE, {
+					["name"] = "Wonderheal XT68 Shades",
+					["classes"] = { DRUID },
+					["recipeID"] = 46106
+				}),
+			}
+		},
+		{
+			["name"] = "Devices",
+			["categoryID"] = 753,
+			["g"] = {
+				{
+					["name"] = "Adamantite Arrow Maker",
+					["timeline"] = { "removed 4.0.1.10000" },
+					["recipeID"] = 43676,
+				},
+				{
+					["name"] = "Adamantite Shell Machine",
+					["timeline"] = { "removed 4.0.1.10000" },
+					["recipeID"] = 30347,
+				},
+				{
+					["name"] = "Crashin' Thrashin' Robot",
+					["recipeID"] = 30337
+				},
+				{
+					["name"] = "Dimensional Ripper - Area 52",
+					["recipeID"] = 36954
+				},
+				{
+					["name"] = "Fel Iron Toolbox",
+					["recipeID"] = 30348
+				},
+				{
+					["name"] = "Field Repair Bot 110G",
+					["recipeID"] = 44391
+				},
+				{
+					["name"] = "Foreman's Enchanted Helmet",
+					["recipeID"] = 30565
+				},
+				{
+					["name"] = "Foreman's Reinforced Helmet",
+					["recipeID"] = 30566
+				},
+				{
+					["name"] = "Gnomish Flame Turret",
+					["recipeID"] = 30568
+				},
+				{
+					["name"] = "Gnomish Poultryizer",
+					["recipeID"] = 30569
+				},
+				{
+					["name"] = "Goblin Rocket Launcher",
+					["recipeID"] = 30563
+				},
+				{
+					["name"] = "Healing Potion Injector",
+					["recipeID"] = 30551
+				},
+				{
+					["name"] = "Mana Potion Injector",
+					["recipeID"] = 30552
+				},
+				{
+					["name"] = "Nigh-Invulnerability Belt",
+					["recipeID"] = 30570
+				},
+				{
+					["name"] = "Rocket Boots Xtreme",
+					["recipeID"] = 30556
+				},
+				{
+					["name"] = "Rocket Boots Xtreme Lite",
+					["recipeID"] = 46697
+				},
+				{
+					["name"] = "Ultrasafe Transporter - Toshley's Station",
+					["recipeID"] = 36955
+				},
+				{
+					["name"] = "Zapthrottle Mote Extractor",
+					["recipeID"] = 30548
+				}
+			}
+		},
+		{
+			["name"] = "Weapons",
+			["categoryID"] = 754,
+			["g"] = {
+				{
+					["name"] = "Adamantite Rifle",
+					["recipeID"] = 30313
+				},
+				{
+					["name"] = "Fel Iron Musket",
+					["recipeID"] = 30312
+				},
+				{
+					["name"] = "Felsteel Boomstick",
+					["recipeID"] = 30314
+				},
+				{
+					["name"] = "Gyro-balanced Khorium Destroyer",
+					["recipeID"] = 41307
+				},
+				{
+					["name"] = "Ornate Khorium Rifle",
+					["recipeID"] = 30315
+				}
+			}
+		},
+		{
+			["name"] = "Scopes",
+			["categoryID"] = 755,
+			["g"] = {
+				{
+					["name"] = "Adamantite Scope",
+					["recipeID"] = 30329
+				},
+				{
+					["name"] = "Khorium Scope",
+					["recipeID"] = 30332
+				},
+				{
+					["name"] = "Stabilized Eternium Scope",
+					["recipeID"] = 30334
+				}
+			}
+		},
+		{
+			["name"] = "Mounts",
+			["categoryID"] = 756,
+			["g"] = {
+				{
+					["name"] = "Flying Machine",
+					["recipeID"] = 44155
+				},
+				{
+					["name"] = "Turbo-Charged Flying Machine",
+					["recipeID"] = 44157
+				}
+			}
+		},
+		{
+			["name"] = "Fireworks",
+			["categoryID"] = 757,
+			["g"] = {
+				{
+					["name"] = "Green Smoke Flare",
+					["recipeID"] = 30344
+				},
+				{
+					["name"] = "Purple Smoke Flare",
+					["recipeID"] = 32814
+				},
+				{
+					["name"] = "White Smoke Flare",
+					["recipeID"] = 30341
+				}
+			}
+		}
 	})),
 	applyclassicphase(WRATH_PHASE_ONE, tier(3, {	-- Wrath of the Lich King
 		
@@ -637,4 +993,221 @@ profession(ENGINEERING, {
 	applyclassicphase(SHADOWLANDS_PHASE_ONE, tier(9, {	-- Shadowlands
 		
 	})),
+});
+
+-- Engineering Item Database
+_.ItemDB = {};
+
+-- Recipe Cache (for Validation)
+local recipeCache, recipeCacheU = {}, {};
+local function cacheRecipes(g)
+	if g and type(g) == "table" then
+		if g.groups then cacheRecipes(g.groups); end
+		if g.g then cacheRecipes(g.g); end
+		local spellID = g.spellID or g.recipeID;
+		if spellID then
+			recipeCache[spellID] = true; 
+			if g.u then recipeCacheU[spellID] = g.u; end
+		end
+		for i,o in ipairs(g) do
+			cacheRecipes(o);
+		end
+	end
+end
+cacheRecipes(_.Professions);
+
+-- Item Recipe Database
+local itemrecipe = function(name, itemID, spellID, phase, timeline)
+	local o = { ["itemID"] = itemID, ["spellID"] = spellID };
+	if type(phase) == "string" then
+		timeline = phase;
+		phase = nil;
+	end
+	if timeline then
+		-- Ensure that the timeline is in a table format.
+		if type(timeline) == "string" then timeline = { timeline }; end
+		if type(timeline) == "table" then o.timeline = timeline; end
+	end
+	if name then
+		-- Ensure that the name is in a string format.
+		if type(name) == "table" then
+			-- #if AFTER CATA
+			name = name[2];
+			-- #else
+			name = name[1];
+			-- #endif
+		end
+		o.name = name;
+	end
+	_.ItemDB[itemID] = phase and applyclassicphase(phase, o) or o;
+	
+	-- Ensure that this recipe's spellID exists in the profession database.
+	if recipeCache and type(timeline) ~= "boolean" then
+		if recipeCache[o.spellID] then
+			-- Grab the phase from the cache.
+			local u = recipeCacheU[o.spellID];
+			if u then
+				if o.u ~= u then
+					print("ITEM RECIPE MISSING U: ", name, o.spellID, u, o.u);
+					o.u = u;
+				end
+			elseif o.u ~= u then
+				print("RECIPE MISSING U: ", name, o.spellID, o.u);
+			end
+		else
+			print("MISSING RECIPE", name, o.spellID);
+		end
+	end
+	return o;
+end
+
+-- Classic Recipes
+itemrecipe("Schematic: Mechanical Squirrel", 4408, 3928);
+itemrecipe("Schematic: Small Seaforium Charge", 4409, 3933);
+itemrecipe("Schematic: Shadow Goggles", 4410, 3940);
+itemrecipe("Schematic: Flame Deflector", 4411, 3944);
+itemrecipe("Schematic: Moonsight Rifle", 4412, 3954);
+itemrecipe("Schematic: Discombobulator Ray", 4413, 3959);
+itemrecipe("Schematic: Portable Bronze Mortar", 4414, 3960);
+itemrecipe("Schematic: Craftsman's Monocle", 4415, 3966);
+itemrecipe("Schematic: Goblin Land Mine", 4416, 3968);
+itemrecipe("Schematic: Large Seaforium Charge", 4417, 3972);
+itemrecipe("Schematic: Flash Bomb", 6672, 8243);
+itemrecipe("Schematic: EZ-Thro Dynamite", 6716, 8339);
+itemrecipe("Schematic: Goblin Rocket Boots", 7192, 8895);
+itemrecipe("Schematic: Gnomish Universal Remote", 7560, 9269);
+itemrecipe("Schematic: Goblin Jumper Cables", 7561, 9273);
+itemrecipe("Schematic: Gnomish Cloaking Device", 7742, 3971);
+itemrecipe("Schematic: Bright-Eye Goggles", 10601, 12587);
+itemrecipe("Schematic: Deadly Scope", 10602, 12597);
+itemrecipe("Schematic: Catseye Ultra Goggles", 10603, 12607);
+itemrecipe("Schematic: Mithril Heavy-bore Rifle", 10604, 12614);
+itemrecipe("Schematic: Spellpower Goggles Xtreme", 10605, 12615);
+itemrecipe("Schematic: Parachute Cloak", 10606, 12616);
+itemrecipe("Schematic: Deepdive Helmet", 10607, 12617);
+itemrecipe("Schematic: Sniper Scope", 10608, 12620);
+itemrecipe("Schematic: Mithril Mechanical Dragonling", 10609, 12624);
+itemrecipe("Schematic: Lil' Smoky", 11827, 15633, nil, true);
+itemrecipe("Schematic: Pet Bombling", 11828, 15628, nil, true);
+itemrecipe("Schematic: Ice Deflector", 13308, 3957);
+itemrecipe("Schematic: Lovingly Crafted Boomstick", 13309, 3939);
+itemrecipe("Schematic: Accurate Scope", 13310, 3979);
+itemrecipe("Schematic: Mechanical Dragonling", 13311, 3969);
+itemrecipe("Schematic: Minor Recombobulator", 14639, 3952);
+itemrecipe("Schematic: Thorium Grenade", 16041, 19790);
+itemrecipe("Schematic: Thorium Widget", 16042, 19791);
+itemrecipe("Schematic: Thorium Rifle", 16043, 19792);
+itemrecipe("Schematic: Lifelike Mechanical Toad", 16044, 19793);
+itemrecipe("Schematic: Spellpower Goggles Xtreme Plus", 16045, 19794);
+itemrecipe("Schematic: Masterwork Target Dummy", 16046, 19814);
+itemrecipe("Schematic: Thorium Tube", 16047, 19795);
+itemrecipe("Schematic: Dark Iron Rifle", 16048, 19796);
+itemrecipe("Schematic: Dark Iron Bomb", 16049, 19799);
+itemrecipe("Schematic: Delicate Arcanite Converter", 16050, 19815);
+itemrecipe("Schematic: Thorium Shells", 16051, 19800);
+itemrecipe("Schematic: Voice Amplification Modulator", 16052, 19819);
+itemrecipe("Schematic: Master Engineer's Goggles", 16053, 19825);
+itemrecipe("Schematic: Arcanite Dragonling", 16054, 19830);
+itemrecipe("Schematic: Arcane Bomb", 16055, 19831);
+itemrecipe("Schematic: Flawless Arcanite Rifle", 16056, 19833);
+un(FEAST_OF_WINTER_VEIL, itemrecipe("Schematic: Snowmaster 9000", 17720, 21940));
+itemrecipe("Schematic: Field Repair Bot 74A", 18235, 22704);
+itemrecipe("Schematic: Biznicks 247x128 Accurascope", 18290, 22793);
+itemrecipe("Schematic: Force Reactive Disk", 18291, 22797);
+itemrecipe("Schematic: Core Marksman Rifle", 18292, 22795);
+itemrecipe("Schematic: Red Firework", 18647, 23066);
+itemrecipe("Schematic: Green Firework", 18648, 23068);
+itemrecipe("Schematic: Blue Firework", 18649, 23067);
+itemrecipe("Schematic: EZ-Thro Dynamite II", 18650, 23069);
+itemrecipe("Schematic: Truesilver Transformer", 18651, 23071);
+itemrecipe("Schematic: Gyrofreeze Ice Reflector", 18652, 23077);
+itemrecipe("Schematic: Major Recombobulator", 18655, 23079, PHASE_ONE_DIREMAUL);
+itemrecipe("Schematic: Powerful Seaforium Charge", 18656, 23080);
+itemrecipe("Schematic: Hyper-Radiant Flame Reflector", 18657, 23081);
+itemrecipe("Schematic: Ultra-Flash Shadow Reflector", 18658, 23082);
+itemrecipe("Schematic: Snake Burst Firework", 19027, 23507);
+itemrecipe("Schematic: Bloodvine Goggles", 20000, 24356, PHASE_FOUR);
+itemrecipe("Schematic: Bloodvine Lens", 20001, 24357, PHASE_FOUR);
+bubbleDown({ ["u"] = LUNAR_FESTIVAL },{
+	itemrecipe("Schematic: Small Blue Rocket", 21724, 26416),
+	itemrecipe("Schematic: Small Green Rocket", 21725, 26417),
+	itemrecipe("Schematic: Small Red Rocket", 21726, 26418),
+	itemrecipe("Schematic: Large Blue Rocket", 21727, 26420),
+	itemrecipe("Schematic: Large Green Rocket", 21728, 26421),
+	itemrecipe("Schematic: Large Red Rocket", 21729, 26422),
+	itemrecipe("Schematic: Blue Rocket Cluster", 21730, 26423),
+	itemrecipe("Schematic: Green Rocket Cluster", 21731, 26424),
+	itemrecipe("Schematic: Red Rocket Cluster", 21732, 26425),
+	itemrecipe("Schematic: Large Blue Rocket Cluster", 21733, 26426),
+	itemrecipe("Schematic: Large Green Rocket Cluster", 21734, 26427),
+	itemrecipe("Schematic: Large Red Rocket Cluster", 21735, 26428),
+	itemrecipe("Schematic: Cluster Launcher", 21737, 26443),
+	itemrecipe("Schematic: Firework Launcher", 21738, 26442),
+});
+
+-- #if AFTER TBC
+-- TBC Recipes
+itemrecipe("Schematic: Steam Tonk Controller", 22729, 28327, TBC_PHASE_ONE);
+itemrecipe("Schematic: Adamantite Rifle", 23799, 30313, TBC_PHASE_ONE);
+itemrecipe("Schematic: Felsteel Boomstick", 23800, 30314, TBC_PHASE_ONE);
+itemrecipe("Schematic: Ornate Khorium Rifle", 23802, 30315, TBC_PHASE_ONE);
+itemrecipe("Schematic: Cogspinner Goggles", 23803, 30316, TBC_PHASE_ONE);
+itemrecipe("Schematic: Power Amplification Goggles", 23804, 30317, TBC_PHASE_ONE);
+itemrecipe("Schematic: Ultra-Spectropic Detection Goggles", 23805, 30318, TBC_PHASE_ONE);
+itemrecipe("Schematic: Hyper-Vision Goggles", 23806, 30325, TBC_PHASE_ONE);
+itemrecipe("Schematic: Adamantite Scope", 23807, 30329, TBC_PHASE_ONE);
+itemrecipe("Schematic: Khorium Scope", 23808, 30332, TBC_PHASE_ONE);
+itemrecipe("Schematic: Stabilized Eternium Scope", 23809, 30334, TBC_PHASE_ONE);
+itemrecipe("Schematic: Crashin' Thrashin' Robot", 23810, 30337, TBC_PHASE_ONE);
+itemrecipe("Schematic: White Smoke Flare", 23811, 30341, TBC_PHASE_ONE);
+itemrecipe("Schematic: Green Smoke Flare", 23814, 30344, TBC_PHASE_ONE);
+itemrecipe("Schematic: Purple Smoke Flare", 25887, 32814, TBC_PHASE_ONE);
+itemrecipe("Schematic: Adamantite Arrow Maker", 33804, 43676, TBC_PHASE_ONE, "removed 4.0.1.10000");
+itemrecipe("Schematic: Adamantite Shell Machine", 23815, 30347, TBC_PHASE_ONE, "removed 4.0.1.10000");
+itemrecipe("Schematic: Fel Iron Toolbox", 23816, 30348, TBC_PHASE_ONE);
+itemrecipe("Schematic: Elemental Seaforium Charge", 23874, 30547, TBC_PHASE_ONE);
+itemrecipe("Schematic: Healing Potion Injector", 23883, 30551, TBC_PHASE_ONE);
+itemrecipe("Schematic: Mana Potion Injector", 23884, 30552, TBC_PHASE_ONE);
+itemrecipe("Schematic: Rocket Boots Xtreme", 23887, 30556, TBC_PHASE_ONE);
+itemrecipe("Schematic: Fused Wiring", 32381, 39895, TBC_PHASE_ONE);
+itemrecipe("Schematic: Zapthrottle Mote Extractor", 23888, 30548, TBC_PHASE_ONE);
+itemrecipe("Schematic: Field Repair Bot 110G", 34114, 44391, TBC_PHASE_ONE);
+
+itemrecipe("Schematic: Annihilator Holo-Gogs", 35186, 46111, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Justicebringer 3000 Specs", 35187, 46107, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Powerheal 9000 Lens", 35189, 46108, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Hyper-Magnified Moon Specs", 35190, 46109, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Wonderheal XT68 Shades", 35191, 46106, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Primal-Attuned Goggles", 35192, 46110, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Lightning Etched Specs", 35193, 46112, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Surestrike Goggles v3.0", 35194, 46113, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Mayhem Projection Goggles", 35195, 46114, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Hard Khorium Goggles", 35196, 46115, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Quad Deathblow X44 Goggles", 35197, 46116, TBC_PHASE_FIVE);
+itemrecipe("Schematic: Healing Potion Injector", 35310, 30551, TBC_PHASE_FIVE, true);
+itemrecipe("Schematic: Mana Potion Injector", 35311, 30552, TBC_PHASE_FIVE, true);
+itemrecipe("Schematic: Rocket Boots Xtreme Lite", 35582, 46697, TBC_PHASE_ONE);
+-- #endif
+
+-- #if AFTER WRATH
+-- Wrath Recipes
+
+-- #endif
+
+
+-- These items never made it in.
+recipeCache = nil;	-- Invalidate the cache.
+_.NeverImplemented = bubbleDown({ ["u"] = NEVER_IMPLEMENTED }, {
+	filter(200, {	-- Recipe
+		-- #if CLASSIC
+		i(22729),	-- Schematic: Steam Tonk Controller [This doesn't get added until TBC]
+		-- #endif
+		-- #if AFTER TBC
+		itemrecipe("Schematic: Red Smoke Flare", 23812, 30342);
+		itemrecipe("Schematic: Blue Smoke Flare", 23813, 30343);
+		itemrecipe("Schematic: Khorium Toolbox", 23817, 30349);	-- Not implemented until Wrath, when it became a Titanium Toolbox
+		itemrecipe("Schematic: Critter Enlarger", 23882, 30549);
+		itemrecipe("Schematic: Remote Mail Terminal", 23885, 30555);
+		-- #endif
+	}),
 });
