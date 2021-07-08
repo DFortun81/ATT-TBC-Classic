8fd2333aef769fa1b8e073128ce01e1ae165ac51
Crieve <dylanfortune@live.com>
2021-07-08 11:40:53 -0700

    Added a blank map for Eye of the Storm.

---
+++ .contrib/.db/09 - PvP/02. Battlegrounds/Eye of the Storm.lua
@@ -0,0 +1,11 @@
+_.PvP =
+{
+	n(-304, {	-- Battlegrounds
+		applyclassicphase(TBC_PHASE_ONE, m(1956, {	-- Eye of the Storm
+			--["description"] = "",
+			["groups"] = {
+				
+			},
+		})),
+	}),
+};
