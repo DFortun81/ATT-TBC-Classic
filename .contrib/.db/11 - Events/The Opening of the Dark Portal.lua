1ba688ccf53bfc4f5e7d08d8b9576b1bd888a74b
Crieve <dylanfortune@live.com>
2021-08-26 21:55:18 -0700

    Updated the formatting of The Opening of the Dark Portal world event.

--- .contrib/.db/11 - Events/The Opening of the Dark Portal.lua
+++ .contrib/.db/11 - Events/The Opening of the Dark Portal.lua
@@ -1,9 +1,8 @@
 --------------------------------------------
 --       E V E N T S    M O D U L E       --
 --------------------------------------------
-_.WorldEvents = bubbleDown({["u"] = TBC_PHASE_ONE_DARKPORTAL },
-{
-	n(-542, {	-- The Opening of the Dark Portal
+_.WorldEvents = { n(-542,	-- The Opening of the Dark Portal
+	applyclassicphase(TBC_PHASE_ONE_DARKPORTAL, bubbleDown({ ["timeline"] = { "removed 3.0.1" } }, {
 		["description"] = "The Opening of the Dark Portal was a one-time event that happened between Prepatch and the launch of TBC and subsequentially removed after. The tabard is the only tabard in the game with the Flex emote built into it and is highly covetted by collectors. Highlord Kruul would spawn randomly outside of the major cities and max level zones of the time.",
 		["maps"] = { BLASTED_LANDS },
 		["lvl"] = 55,
@@ -13,9 +12,10 @@ _.WorldEvents = bubbleDown({["u"] = TBC_PHASE_ONE_DARKPORTAL },
 				["coord"] = { 58.25, 55.05, BLASTED_LANDS },
 				["lvl"] = 55,
 				["groups"] = {
+					ach(2079),	-- Tabard of the Protector
 					i(28788),	-- Tabard of the Protector
 				},
 			}),
 		},
-	}),
-});
+	}))
+)};
