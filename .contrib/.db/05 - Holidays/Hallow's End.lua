170c87a7ce28e5c55ece89b7b09aff42d86ad860
Crieve <dylanfortune@live.com>
2021-08-30 09:18:43 -0700

    Slightly updated the format of Hallow's End.

--- .contrib/.db/05 - Holidays/Hallow's End.lua
+++ .contrib/.db/05 - Holidays/Hallow's End.lua
@@ -1,68 +1,71 @@
 --------------------------------------------
 --     H O L I D A Y S  M O D U L E       --
 --------------------------------------------
-_.Holidays = bubbleDown({ ["u"] = HALLOWS_END },
-{
-	n(-58, {	-- Hallow's End
+_.Holidays = { applyholiday(HALLOWS_END, {
+	-- #if ANYCLASSIC
+	["npcID"] = -58,
+	-- #else
+	["holidayID"] = 235462,
+	-- #endif
+	["groups"] = {
 		n(QUESTS, {
 			q(8353, {	-- Chicken Clucking for a Mint
-				["lvl"] = 10,
-				["races"] = ALLIANCE_ONLY,
 				["qg"] = 5111,	-- Innkeeper Firebrew <Innkeeper>
+				["races"] = ALLIANCE_ONLY,
 				["repeatable"] = true,
-				["g"] = {
+				["lvl"] = 10,
+				["groups"] = {
 					i(20490),	-- Ironforge Mint
 				},
 			}),
 			q(8354, {	-- Chicken Clucking for a Mint
-				["lvl"] = 10,
-				["races"] = HORDE_ONLY,
 				["qg"] = 6741,	-- Innkeeper Norman <Innkeeper>
+				["races"] = HORDE_ONLY,
 				["repeatable"] = true,
-				["g"] = {
+				["lvl"] = 10,
+				["groups"] = {
 					i(20491),	-- Undercity Mint
 				},
 			}),
 			q(8357, {	-- Dancing for Marzipan
-				["lvl"] = 10,
-				["races"] = ALLIANCE_ONLY,
 				["qg"] = 6735,	-- Innkeeper Saelienne <Innkeeper>
+				["races"] = ALLIANCE_ONLY,
 				["repeatable"] = true,
-				["g"] = {
+				["lvl"] = 10,
+				["groups"] = {
 					i(20496),	-- Darnassus Marzipan
 				},
 			}),
 			q(8360, {	-- Dancing for Marzipan
-				["lvl"] = 10,
-				["races"] = HORDE_ONLY,
 				["qg"] = 6746,	-- Innkeeper Pala <Innkeeper>
+				["races"] = HORDE_ONLY,
 				["repeatable"] = true,
-				["g"] = {
+				["lvl"] = 10,
+				["groups"] = {
 					i(20497),	-- Thunder Bluff Marzipan
 				},
 			}),
 			q(8356, {	-- Flexing for Nougat
-				["lvl"] = 10,
-				["races"] = HORDE_ONLY,
 				["qg"] = 6740,	-- Innkeeper Allison <Innkeeper>
+				["races"] = HORDE_ONLY,
 				["repeatable"] = true,
-				["g"] = {
+				["lvl"] = 10,
+				["groups"] = {
 					i(20492),	-- Stormwind Nougat
 				},
 			}),
 			q(8359, {	-- Flexing for Nougat
-				["lvl"] = 10,
-				["races"] = HORDE_ONLY,
 				["qg"] = 6929,	-- Innkeeper Gryshka <Innkeeper>
+				["races"] = HORDE_ONLY,
 				["repeatable"] = true,
-				["g"] = {
+				["lvl"] = 10,
+				["groups"] = {
 					i(20493),	-- Orgrimmar Nougat
 				},
 			}),
 			q(8311, {	-- Hallow's End Treats for Jesper!
-				["lvl"] = 10,
-				["races"] = ALLIANCE_ONLY,
 				["qg"] = 15310,	-- Jesper
+				["races"] = ALLIANCE_ONLY,
 				["repeatable"] = true,
 				["cost"] = {
 					{ "i", 20492, 1 },	-- Stormwind Nougat
@@ -70,14 +73,14 @@ _.Holidays = bubbleDown({ ["u"] = HALLOWS_END },
 					{ "i", 20490, 1 },	-- Ironforge Mint
 					{ "i", 20496, 1 },	-- Darnassus Marzipan
 				},
-				["g"] = {
+				["lvl"] = 10,
+				["groups"] = {
 					i(20557),	-- Hallow's End Pumpkin Treat
 				},
 			}),
 			q(8312, {	-- Hallow's End Treats for Spoops!
-				["lvl"] = 10,
-				["races"] = HORDE_ONLY,
 				["qg"] = 15309,	-- Spoops
+				["races"] = HORDE_ONLY,
 				["repeatable"] = true,
 				["cost"] = {
 					{ "i", 20493, 1 },	-- Orgrimmar Nougat
@@ -85,62 +88,63 @@ _.Holidays = bubbleDown({ ["u"] = HALLOWS_END },
 					{ "i", 20491, 1 },	-- Undercity Mint
 					{ "i", 20497, 1 },	-- Thunder Bluff Marzipan
 				},
-				["g"] = {
+				["lvl"] = 10,
+				["groups"] = {
 					i(20557),	-- Hallow's End Pumpkin Treat
 				},
 			}),
 			q(8355, {	-- Incoming Gumdrop
-				["lvl"] = 10,
-				["races"] = ALLIANCE_ONLY,
 				["qg"] = 6826,	-- Talvash del Kissel
+				["races"] = ALLIANCE_ONLY,
 				["repeatable"] = true,
-				["g"] = {
+				["lvl"] = 10,
+				["groups"] = {
 					i(20494),	-- Gnomeregan Gumdrop
 				},
 			}),
 			q(8358, {	-- Incoming Gumdrop
-				["lvl"] = 10,
-				["races"] = HORDE_ONLY,
 				["qg"] = 11814,	-- Kali Remik
+				["races"] = HORDE_ONLY,
 				["repeatable"] = true,
-				["g"] = {
+				["lvl"] = 10,
+				["groups"] = {
 					i(20495),	-- Darkspear Gumdrop
 				},
 			}),
 			q(8322, {	-- Rotten Eggs
-				["lvl"] = 30,
-				["races"] = HORDE_ONLY,
 				["qg"] = 15197,	-- Darkcaller Yanka
+				["races"] = HORDE_ONLY,
 				["repeatable"] = true,
+				["lvl"] = 30,
 			}),
 			q(8409, {	-- Ruined Kegs
-				["lvl"] = 25,
-				["races"] = HORDE_ONLY,
 				["provider"] = { "o", 180570 },	-- Keg
 				["sourceQuest"] = 8322,	-- Rotten Eggs
+				["races"] = HORDE_ONLY,
 				["repeatable"] = true,
+				["lvl"] = 25,
 			}),
 			q(1657, {	-- Stinking Up Southshore
-				["lvl"] = 25,
-				["races"] = HORDE_ONLY,
 				["qg"] = 15197,	-- Darkcaller Yanka
+				["races"] = HORDE_ONLY,
 				["repeatable"] = true,
+				["lvl"] = 25,
 			}),
 			q(1658, {	-- Crashing the Wickerman Festival
-				["lvl"] = 25,
-				["races"] = ALLIANCE_ONLY,
 				["qg"] = 15199,	-- Sergeant Hartman
+				["races"] = ALLIANCE_ONLY,
 				["isYearly"] = true,
+				["lvl"] = 25,
 			}),
 			q(8373, {	-- The Power of Pine
-				["lvl"] = 25,
-				["races"] = ALLIANCE_ONLY,
 				["qg"] = 15199,	-- Sergeant Hartman
+				["races"] = ALLIANCE_ONLY,
 				["repeatable"] = true,
+				["lvl"] = 25,
 			}),
 		}),
 		n(ZONEDROPS, {
 			i(20400),	-- Pumpkin Bag
 		}),
-	}),
-});
+	},
+})};
