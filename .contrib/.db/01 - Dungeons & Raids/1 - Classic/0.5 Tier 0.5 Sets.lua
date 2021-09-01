ccd8d1f928f9c960e48c47b4a217ed1b04c9f585
Crieve <dylanfortune@live.com>
2021-09-01 02:10:44 -0700

    I think I got all of the description content that was actually just classified as Lore all along.

--- .contrib/.db/01 - Dungeons & Raids/1 - Classic/0.5 Tier 0.5 Sets.lua
+++ .contrib/.db/01 - Dungeons & Raids/1 - Classic/0.5 Tier 0.5 Sets.lua
@@ -3,7 +3,7 @@
 -----------------------------------------------------
 _.Instances = { tier(CLASSIC_TIER, applylegacyclassicphase(PHASE_FIVE, {
 	n(-420, {	-- Tier 0.5 Sets
-		["description"] = "The Dungeon Set 2 class sets, commonly referred to as Tier 0.5, are obtained by completing a long quest chain to upgrade the first set available as drops in end game dungeons into stronger versions of themselves. In current WoW, these sets are covetted by Collectors as the quest chain was completely removed from the game with Cataclysm. In WoW Classic, you should finish this quest chain on all of your characters before then!",
+		["lore"] = "The Dungeon Set 2 class sets, commonly referred to as Tier 0.5, are obtained by completing a long quest chain to upgrade the first set available as drops in end game dungeons into stronger versions of themselves. In current WoW, these sets are covetted by Collectors as the quest chain was completely removed from the game with Cataclysm. In WoW Classic, you should finish this quest chain on all of your characters before then!",
 		["groups"] = {
 			cl(DRUID, {
 				q(8905,	{	-- An Earnest Proposition [ALLIANCE]
--- .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Tanaris/0 - Caverns of Time/0 - Metadata.lua
+++ .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Tanaris/0 - Caverns of Time/0 - Metadata.lua
@@ -6,7 +6,7 @@ _.Zones =
 	m(KALIMDOR, {
 		m(TANARIS, {
 			m(CAVERNS_OF_TIME, {
-				["description"] = "Located in the eastern part of Tanaris, the Caverns of Time contain various portals to different key historical periods and events in Warcraft history.",
+				["lore"] = "Located in the eastern part of Tanaris, the Caverns of Time contain various portals to different key historical periods and events in Warcraft history.",
 				["maps"] = { 74 },	-- 	Caverns of Time Entrance
 				-- #if AFTER WRATH
 				["icon"] = 2026009,
