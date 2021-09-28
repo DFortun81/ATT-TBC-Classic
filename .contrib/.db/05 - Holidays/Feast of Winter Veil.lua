b7a43c36129db21f622e97fa95ae54ccb1fb40b8
Crieve <dylanfortune@live.com>
2021-09-27 20:33:06 -0700

    Added all future bosses for the Winter Hats.

--- .contrib/.db/05 - Holidays/Feast of Winter Veil.lua
+++ .contrib/.db/05 - Holidays/Feast of Winter Veil.lua
@@ -1,32 +1,234 @@
 --------------------------------------------
 --     H O L I D A Y S  M O D U L E       --
 --------------------------------------------
-_.Holidays = bubbleDown({ ["u"] = FEAST_OF_WINTER_VEIL },
-{
-	n(-61, {	-- Feast of Winter Veil
+_.Holidays = { applyholiday(FEAST_OF_WINTER_VEIL, {
+	-- #if ANYCLASSIC
+	["npcID"] = -61,
+	-- #else
+	["holidayID"] = 235485,
+	-- #endif
+	["groups"] = {
 		n(COMMON_BOSS_DROPS, {
-			i(21525, {	-- Green Winter Hat
+			-- #if AFTER LEGION
+			i(139300, {	-- Finely-Tailored Green Holiday Hat
+				["description"] = "In order to mark this item as collected you will need to do a Shift+Click on the header.",
 				["maps"] = {
-					BLACKROCK_SPIRE,
-					DIRE_MAUL,
-					SCHOLOMANCE
+					909, 910, 911, 912, 913, 914, 915, 916, 917, 918, 919, 920,	-- Antorus
+					751, 752, 753, 754, 755, 756,	-- Black Rook Hold
+					713,	-- Eye of Azshara
+					-- #if AFTER BFA
+					1038,	-- Temple of Sethraliss
+					1043,	-- Atrium of Sethraliss
+					-- Waycrest Manor
+					1015,	-- The Grand Foyer
+					1016,	-- The Cellar
+					1017,	-- Catacombs
+					1018,	-- The Rupture
+					1029,	-- Waycrest Dimension
+					1512, 1513, 1514, 1515, 1516, 1517, 1518, 1519, 1520,	-- The Eternal Palace
+					-- #endif
 				},
 				["crs"] = {
+					125075,	-- Varimathras
+					98696,	-- Illysanna Ravencrest
+					91797,	-- King Deepbeard
+					-- #if AFTER BFA
+					133944,	-- Aspix
+					131863,	-- Raal the Gluttonous
+					150653,	-- Blackwater Behemoth
+					-- #endif
+				},
+			}),
+			i(139299, {	-- Finely-Tailored Red Holiday Hat
+				["description"] = "In order to mark this item as collected you will need to do a Shift+Click on the header.",
+				["maps"] = {
+					764, 765, 766, 767, 768, 769, 770, 771, 772,	-- Nighthold
+					761, 763,	-- Court of Stars
+					903,	-- The Seat of the Triumvirate
+					-- #if AFTER BFA
+					1148,	-- Ruin's Descent
+					1149,	-- Hall of Sanitation
+					1150,	-- Ring of Containment
+					1151,	-- Archives of Eternity
+					1152,	-- Plague Vault
+					1153,	-- Gallery of Failures
+					1154,	-- The Oblivion Door
+					1155,	-- The Festering Core
+					934,	-- Atal'Dazar
+					935,	-- Sacrificial Pits
+					936,	-- Freehold
+					-- #endif
+				},
+				["crs"] = {
+					101002,	-- Krosus
+					104217,	-- Talixae Flamewreath
+					122316,	-- Saprish
+					-- #if AFTER BFA
+					133298,	-- Fetid Devourer
+					122965,	-- Vol'kaal
+					126832,	-- Skycap'n Kragg
+					-- #endif
+				},
+			}),
+			-- #endif
+			i(21525, {	-- Green Winter Hat
+				["maps"] = {
+					-- #if AFTER SHADOWLANDS
+					-- Castle Nathria
+					1735,	-- The Grand Walk
+					1744,	-- The Purloined Stores
+					1745,	-- Halls of the Faithful
+					1746,	-- Pride's Prison
+					1747,	-- Nightcloak Sanctum
+					1748,	-- The Observatorium
+					1750,	-- Feast of Arrogance
+					-- #endif
+					-- #if AFTER WOD
+					593,	-- Auchindoun
+					595,	-- Iron Docks
+					610, 611, 612, 613, 614, 615,	-- Highmaul
+					596, 597, 598, 599, 600,	-- Blackrock Foundry
+					661, 662, 663, 664, 665, 666, 667, 668, 669, 670,	-- Hellfire Citadel
+					-- #endif
+					-- #if AFTER MOP
+					556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570,	-- Siege of Orgrimmar
+					-- #endif
+					-- #if AFTER CATA
+					293,	-- Grim Batol
+					SHADOWFANG_KEEP,
+					SHADOWFANG_KEEP_LEVEL2,
+					SHADOWFANG_KEEP_LEVEL3,
+					SHADOWFANG_KEEP_LEVEL4,
+					SHADOWFANG_KEEP_LEVEL5,
+					SHADOWFANG_KEEP_LEVEL6,
+					SHADOWFANG_KEEP_LEVEL7,
+					-- #endif
+					-- #if AFTER WRATH
+					142, 143, 144, 145, 146,	-- The Oculus
+					-- #else
+					BLACKROCK_SPIRE,
+					DIRE_MAUL,
+					SCHOLOMANCE,
+					-- #if AFTER TBC
+					AUCHINDOUN_AUCHENAI_CRYPTS,
+					HELLFIRE_CITADEL_SHATTERED_HALLS,
+					AUCHINDOUN_SHADOW_LABYRINTH,
+					-- #endif
+					-- #endif
+				},
+				["crs"] = {
+					-- #if AFTER SHADOWLANDS
+					166969,	-- Baroness Frieda
+					-- #endif
+					-- #if AFTER WOD
+					76177,	-- Soulbinder Nyami
+					79852,	-- Oshir
+					77404,	-- The Butcher
+					76865,	-- Beastlord Darmac
+					93439,	-- Tyrant Velhari
+					-- #endif
+					-- #if AFTER MOP
+					72249,	-- Galakras <The Last Brood of Galakrond>
+					-- #endif
+					-- #if AFTER CATA
+					40319,	-- Drahga Shadowburner
+					46964,	-- Lord Godfrey
+					-- #endif
+					-- #if AFTER WRATH
+					27655,	-- Mage-Lord Urom
+					-- #else
+					1853,	-- Darkmaster Gandling
 					10899,	-- Goraluk Anvilcrack <Blackhand Legion Armorsmith>
 					11486,	-- Prince Tortheldrin
-					1853,	-- Darkmaster Gandling
+					-- #if AFTER TBC
+					10429,	-- Warchief Rend Blackhand (TBC only)
+					18373,	-- Exarch Maladaar
+					16807,	-- Grand Warlock Nethekurse
+					18732,	-- Grandmaster Vorpil
+					-- #endif
+					-- #endif
 				},
 			}),
 			i(21524, {	-- Red Winter Hat
 				["maps"] = {
+					-- #if AFTER SHADOWLANDS
+					-- Castle Nathria
+					1735,	-- The Grand Walk
+					1744,	-- The Purloined Stores
+					1745,	-- Halls of the Faithful
+					1746,	-- Pride's Prison
+					1747,	-- Nightcloak Sanctum
+					1748,	-- The Observatorium
+					1750,	-- Feast of Arrogance
+					1674,	-- Plaguefall
+					1697,	-- Plaguefall
+					1692,	-- Honor's Ascent
+					1693,	-- Gardens of Repose
+					1694,	-- Font of Fealty
+					1695,	-- Seat of the Archon
+					-- #endif
+					-- #if AFTER WOD
+					573,	-- Bloodmaul Slag Mines
+					BLACKROCK_SPIRE, 617, 618,	-- Upper Blackrock Spire
+					596, 597, 598, 599, 600,	-- Blackrock Foundry
+					661, 662, 663, 664, 665, 666, 667, 668, 669, 670,	-- Hellfire Citadel
+					-- #endif
+					-- #if AFTER MOP
+					556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570,	-- Siege of Orgrimmar
+					-- #endif
+					-- #if AFTER CATA
+					291, 292,	-- Deadmines
+					283, 284,	-- Blackrock Caverns
+					324,	-- The Stonecore
+					-- #endif
+					-- #if AFTER WRATH
+					129,	-- The Nexus
+					132,	-- Ahn'kahet: The Old Kingdom
+					-- #else
 					BLACKROCK_DEPTHS,
 					BLACKROCK_SPIRE,
-					STRATHOLME
+					STRATHOLME,
+					-- #if AFTER TBC
+					CAVERNS_OF_TIME_OLD_HILLSBRAD_FOOTHILLS,
+					TEMPEST_KEEP_BOTANICA,
+					TEMPEST_KEEP_MECHANAR,
+					-- #endif
+					-- #endif
 				},
 				["crs"] = {
-					9019,	-- Emperor Dagran Thaurissan (BRD)
-					9237,	-- War Master Voone
+					-- #if AFTER SHADOWLANDS
+					166969,	-- Baroness Frieda
+					167406,	-- Sire Denathrius
+					164255,	-- Globgrog
+					162061,	-- Devos <Paragon of Doubt>
+					-- #endif
+					-- #if AFTER WOD
+					74790,	-- Gug'rokk
+					79912,	-- Commander Tharbek
+					76814,	-- Flamebender Ka'graz
+					90284,	-- Iron Reaver
+					-- #endif
+					-- #if AFTER MOP
+					71515,	-- General Nazgrim
+					-- #endif
+					-- #if AFTER CATA
+					47626,	-- Admiral Ripsnarl
+					39679,	-- Corla, Herald of Twilight
+					42333,	-- High Priestess Azil
+					-- #endif
+					-- #if AFTER WRATH
+					26731,	-- Grand Magus Telestra
+					29310,	-- Jedoga Shadowseeker
+					-- #else
 					10997,	-- Cannon Master Willey
+					9019,	-- Emperor Dagran Thaurissan
+					9237,	-- War Master Voone
+					-- #if AFTER TBC
+					17862,	-- Captain Skarloc
+					17975,	-- High Botanist Freywinn
+					19221,	-- Nethermancer Sepethrea
+					-- #endif
+					-- #endif
 				},
 			}),
 		}),
@@ -99,7 +301,7 @@ _.Holidays = bubbleDown({ ["u"] = FEAST_OF_WINTER_VEIL },
 				["qg"] = 13418,	-- Kaymard Copperpinch <Smokywood Pastures>
 				["races"] = HORDE_ONLY,
 				["lvl"] = 30,
-				["g"] = {
+				["groups"] = {
 					i(17726),	-- Smokywood Pastures Special Gift
 				},
 			}),
@@ -108,7 +310,7 @@ _.Holidays = bubbleDown({ ["u"] = FEAST_OF_WINTER_VEIL },
 				["qg"] = 13433,	-- Wulmort Jinglepocket <Smokywood Pastures>
 				["races"] = ALLIANCE_ONLY,
 				["lvl"] = 30,
-				["g"] = {
+				["groups"] = {
 					i(17726, {	-- Smokywood Pastures Special Gift
 						i(17725),	-- Formula: Enchant Weapon - Winter's Might
 						i(17722),	-- Pattern: Gloves of the Greatfather
@@ -161,7 +363,7 @@ _.Holidays = bubbleDown({ ["u"] = FEAST_OF_WINTER_VEIL },
 				["qg"] = 13418,	-- Kaymard Copperpinch <Smokywood Pastures>
 				["races"] = HORDE_ONLY,
 				["lvl"] = 40,
-				["g"] = {
+				["groups"] = {
 					i(21213),  -- Preserved Holly
 				},
 			}),
@@ -170,7 +372,7 @@ _.Holidays = bubbleDown({ ["u"] = FEAST_OF_WINTER_VEIL },
 				["qg"] = 13433,	-- Wulmort Jinglepocket <Smokywood Pastures>
 				["races"] = ALLIANCE_ONLY,
 				["lvl"] = 40,
-				["g"] = {
+				["groups"] = {
 					i(21213),  -- Preserved Holly
 				},
 			}),
@@ -214,7 +416,7 @@ _.Holidays = bubbleDown({ ["u"] = FEAST_OF_WINTER_VEIL },
 				["requireSkill"] = COOKING,
 				["races"] = ALLIANCE_ONLY,
 				["lvl"] = 40,
-				["g"] = {
+				["groups"] = {
 					i(21213),  -- Preserved Holly
 				},
 			}),
@@ -235,7 +437,7 @@ _.Holidays = bubbleDown({ ["u"] = FEAST_OF_WINTER_VEIL },
 				["qg"] = 13445,	-- Great-father Winter
 				["races"] = HORDE_ONLY,
 				["lvl"] = 10,
-				["g"] = {
+				["groups"] = {
 					i(17727),	-- Smokywood Pastures Special Gift
 				},
 			}),
@@ -244,7 +446,7 @@ _.Holidays = bubbleDown({ ["u"] = FEAST_OF_WINTER_VEIL },
 				["qg"] = 13444,	-- Greatfather Winter
 				["races"] = ALLIANCE_ONLY,
 				["lvl"] = 10,
-				["g"] = {
+				["groups"] = {
 					i(17727, {	-- Smokywood Pastures Special Gift
 						i(17725),	-- Formula: Enchant Weapon - Winter's Might
 						i(17722),	-- Pattern: Gloves of the Greatfather
@@ -290,5 +492,5 @@ _.Holidays = bubbleDown({ ["u"] = FEAST_OF_WINTER_VEIL },
 				},
 			}),
 		}),
-	}),
-});
+	},
+})};
