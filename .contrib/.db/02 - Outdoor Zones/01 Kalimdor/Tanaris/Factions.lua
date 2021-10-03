415f367c00c6e1380d3244f86aaf429c22cc0e70
Crieve <dylanfortune@live.com>
2021-10-03 16:52:33 -0700

    Added Goblin Reputation tooltips for all of the Goblin Factions.

--- .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Tanaris/Factions.lua
+++ .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Tanaris/Factions.lua
@@ -1,6 +1,29 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
+local OnTooltipForGadgetzhan = [[function(t)
+	local reputation = t.reputation;
+	if reputation < 42000 then
+		local isHuman = _.RaceIndex == 1;
+		if reputation < 0 then
+			local repPerKill = isHuman and 5.5 or 5;
+			local x, n = math.ceil((42000 - t.reputation) / repPerKill), math.ceil(84000 / repPerKill);
+			GameTooltip:AddDoubleLine("Kill Southsea Pirates in Tanaris", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+			
+			local repPerKill = isHuman and 2.25 or 2;
+			local x, n = math.ceil((42000 - t.reputation) / repPerKill), math.ceil(84000 / repPerKill);
+			GameTooltip:AddDoubleLine("Kill Pirates near Ratchet", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+		else
+			local repPerKill = isHuman and 5.5 or 5;
+			local x, n = math.ceil((42000 - t.reputation) / repPerKill), math.ceil(42000 / repPerKill);
+			GameTooltip:AddDoubleLine("Kill Southsea Pirates in Tanaris", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+			
+			local repPerKill = isHuman and 2.25 or 2;
+			local x, n = math.ceil((42000 - t.reputation) / repPerKill), math.ceil(42000 / repPerKill);
+			GameTooltip:AddDoubleLine("Kill Pirates near Ratchet", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+		end
+	end
+end]];
 _.Zones =
 {
 	m(KALIMDOR, {
@@ -8,6 +31,7 @@ _.Zones =
 			n(FACTIONS, {
 				faction(369, { 	-- Gadgetzan
 					["icon"] = icon("INV_Misc_Coin_01"),
+					["OnTooltip"] = OnTooltipForGadgetzhan,
 				}),
 			}),
 		}),
--- .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Winterspring/Factions.lua
+++ .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Winterspring/Factions.lua
@@ -1,6 +1,23 @@
 ---------------------------------------------------
 --          Z O N E S        M O D U L E         --
 ---------------------------------------------------
+local OnTooltipForEverlook = [[function(t)
+	local reputation = t.reputation;
+	if reputation < 42000 then
+		local isHuman = _.RaceIndex == 1;
+		if reputation < 0 then
+			local repPerKill = isHuman and 2.25 or 2;
+			local x, n = math.ceil((42000 - t.reputation) / repPerKill), math.ceil(84000 / repPerKill);
+			GameTooltip:AddDoubleLine("Kill Southsea Pirates in Tanaris", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+			GameTooltip:AddDoubleLine("Kill Pirates near Ratchet", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+		else
+			local repPerKill = isHuman and 2.25 or 2;
+			local x, n = math.ceil((42000 - t.reputation) / repPerKill), math.ceil(42000 / repPerKill);
+			GameTooltip:AddDoubleLine("Kill Southsea Pirates in Tanaris", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+			GameTooltip:AddDoubleLine("Kill Pirates near Ratchet", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+		end
+	end
+end]];
 _.Zones =
 {
 	m(KALIMDOR, {
@@ -8,6 +25,7 @@ _.Zones =
 			n(FACTIONS, {
 				faction(577, { 	-- Everlook
 					["icon"] = icon("INV_Misc_Coin_01"),
+					["OnTooltip"] = OnTooltipForEverlook,
 				}),
 				faction(576, {	-- Timbermaw Hold
 					["maps"] = { FELWOOD },
