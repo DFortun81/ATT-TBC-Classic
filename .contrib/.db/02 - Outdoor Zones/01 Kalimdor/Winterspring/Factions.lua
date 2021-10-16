d621c183920456319de326cee9876a58f3e9c39d
Crieve <dylanfortune@live.com>
2021-10-16 01:15:03 -0700

    Included the Classic reputation values for Wintersaber Trainers.

--- .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Winterspring/Factions.lua
+++ .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Winterspring/Factions.lua
@@ -23,7 +23,11 @@ local OnTooltipForWintersaberTrainers = [[function(t)
 	local reputation = t.reputation;
 	if reputation < 42000 then
 		local isHuman = _.RaceIndex == 1;
+		-- #if AFTER TBC
 		local repPerKill = isHuman and 275 or 250;
+		-- #else
+		local repPerKill = isHuman and 55 or 50;
+		-- #endif
 		if reputation < 1500 then
 			local x, n = math.ceil((1500 - t.reputation) / repPerKill), math.ceil(1500 / repPerKill);
 			GameTooltip:AddDoubleLine("Complete Frostsaber Provisions (to 1500)", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
@@ -32,7 +36,11 @@ local OnTooltipForWintersaberTrainers = [[function(t)
 			GameTooltip:AddDoubleLine("Complete Winterfall Intrusion", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
 			
 			if reputation >= ]] .. HONORED .. [[ then
+				-- #if AFTER TBC
 				repPerKill = isHuman and 385 or 350;
+				-- #else
+				repPerKill = isHuman and 55 or 50;
+				-- #endif
 				local x, n = math.ceil((42000 - t.reputation) / repPerKill), math.ceil(42000 / repPerKill);
 				GameTooltip:AddDoubleLine("Complete Rampaging Giants", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
 			end
