d41a2d67a28f719b19d2b0be07475debee68dcb5
Crieve <dylanfortune@live.com>
2021-11-13 19:33:03 -0700

    Added the achievement for killing Kael'Thas.

--- locales/enUS.lua
+++ locales/enUS.lua
@@ -257,6 +257,7 @@ app.L = {
 		[638] = { 168, "Uldaman", app.asset("achievement_boss_archaedas") },
 		[688] = { 168, "Zul'Gurub", app.asset("achievement_boss_hakkar") },
 		[691] = { 168, "Zul'Aman", app.asset("achievement_boss_zuljin") },
+		[696] = { 168, "Tempest Keep", app.asset("achievement_character_bloodelf_male") },
 		[697] = { 168, "The Black Temple", app.asset("achievement_boss_illidan") },
 		
 		[121] = { 170, "Journeyman Cook", "Interface\\Icons\\inv_misc_food_15" },
