99ae5989c1532c4d74def7fdc23dbb01fe355db0
Crieve <dylanfortune@live.com>
2022-01-22 20:27:05 -0700

    Added a fancy tooltip for Timbermaw Hold.

--- .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Winterspring/Factions.lua
+++ .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Winterspring/Factions.lua
@@ -19,6 +19,58 @@ local OnTooltipForEverlook = [[function(t)
 		GameTooltip:AddLine(" * PROTIP: Ratchet is faster.", 1, 1, 1);
 	end
 end]];
+local OnTooltipForTimbermawHold = [[function(t)
+	local reputation = t.reputation;
+	if reputation < 42000 then
+		local isHuman = _.RaceIndex == 1;
+		-- #if BEFORE CATA
+		if reputation >= ]] .. NEUTRAL .. [[ then
+			if not t.deadwood then
+				local f = _.SearchForField("questID", 8470);
+				if f and #f > 0 then t.deadwood = f[1]; end
+			end
+			if not t.winterfall then
+				local f = _.SearchForField("questID", 8471);
+				if f and #f > 0 then t.winterfall = f[1]; end
+			end
+			if not t.deadwood.collected then GameTooltip:AddLine("Complete 'Deadwood Ritual Totem'.", 1, 1, 1); end
+			if not t.winterfall.collected then GameTooltip:AddLine("Complete 'Winterfall Ritual Totem'.", 1, 1, 1); end
+		end
+		-- #endif
+		-- #if AFTER CATA
+		local repPerKill = isHuman and 22 or 20;
+		local x, n = math.ceil((42000 - t.reputation) / repPerKill), math.ceil(84000 / repPerKill);
+		GameTooltip:AddDoubleLine("Kill Deadwood or Winterfall Furbolgs", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+		-- #elseif AFTER TBC
+		if reputation < ]] .. REVERED .. [[ then
+			local repPerKill = isHuman and 11 or 10;
+			local x, n = math.ceil((]] .. REVERED .. [[ - t.reputation) / repPerKill), math.ceil(]] .. (REVERED + 42000) .. [[ / repPerKill);
+			GameTooltip:AddDoubleLine("Kill Deadwood or Winterfall Furbolgs (Stops at Revered)", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+		end
+		-- #else
+		if reputation < ]] .. HONORED .. [[ then
+			local repPerKill = isHuman and 5.5 or 5;
+			local x, n = math.ceil((]] .. HONORED .. [[ - t.reputation) / repPerKill), math.ceil(]] .. (HONORED + 42000) .. [[ / repPerKill);
+			GameTooltip:AddDoubleLine("Kill Deadwood or Winterfall Furbolgs (Stops at Honored)", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+		end
+		-- #endif
+		-- #if AFTER CATA
+		local repPerTurnIn = isHuman and 2200 or 2000;
+		-- #elseif AFTER TBC
+		local repPerTurnIn = isHuman and 165 or 150;
+		-- #else
+		local repPerTurnIn = isHuman and 55 or 50;
+		-- #endif
+		local x, n = math.ceil((42000 - t.reputation) / repPerTurnIn), math.ceil(84000 / repPerTurnIn);
+		GameTooltip:AddDoubleLine("Turn in Deadwood Feathers (x5) in Felwood", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+		GameTooltip:AddDoubleLine("Turn in Winterfall Beads (x5) in Winterspring", (n - x) .. " / " .. n .. " (" .. x .. ")", 1, 1, 1);
+		-- #if BEFORE CATA
+		if reputation < ]] .. HONORED .. [[ then
+			GameTooltip:AddLine("PROTIP: Do NOT turn in the totems or feathers until after Honored!", 1, 0.2, 0.2);
+		end
+		-- #endif
+	end
+end]];
 local OnTooltipForWintersaberTrainers = [[function(t)
 	local reputation = t.reputation;
 	if reputation < 42000 then
@@ -61,6 +113,7 @@ _.Zones =
 					},
 				}),
 				faction(576, {	-- Timbermaw Hold
+					["OnTooltip"] = OnTooltipForTimbermawHold,
 					["maps"] = { FELWOOD },
 				}),
 				faction(589, {	-- Wintersaber Trainers
