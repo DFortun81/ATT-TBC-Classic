18e691ecd184b92ff327e5c2e1e1943f2ac9218b
Crieve <dylanfortune@live.com>
2022-02-01 02:33:49 -0700

    Added some missing Caverns of Time quests.

--- .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Tanaris/0 - Caverns of Time/Quests.lua
+++ .contrib/.db/02 - Outdoor Zones/01 Kalimdor/Tanaris/0 - Caverns of Time/Quests.lua
@@ -6,8 +6,18 @@ _.Zones =
 	m(KALIMDOR, {
 		m(TANARIS, {
 			m(CAVERNS_OF_TIME, {
-				n(QUESTS, applyclassicphase(TBC_PHASE_TWO, {
-					q(10445, {	-- The Vials of Eternity
+				n(QUESTS, {	-- 
+					applyclassicphase(TBC_PHASE_ONE, q(10277, {	-- The Caverns of Time
+						["qg"] = 20130,	-- Andormu <Keepers of Time>
+						["sourceQuest"] = 10279,	-- To The Master's Lair
+						-- #if AFTER CATA
+						["coord"] = { 58, 54, TANARIS },
+						-- #else
+						["coord"] = { 58.4, 54.2, TANARIS },
+						-- #endif
+						["lvl"] = lvlsquish(66, 66, 15),
+					})),
+					applyclassicphase(TBC_PHASE_TWO, q(10445, {	-- The Vials of Eternity
 						["providers"] = {
 							{ "n", 19935 },	-- Soridormi
 							{ "n", 19936 },	-- Arazmodu
@@ -27,7 +37,7 @@ _.Zones =
 								["provider"] = { "i", 29905 },	-- Kael's Vial Remnant
 							}),
 						},
-					}),
+					})),
 					q(13432, {	-- The Vials of Eternity
 						["providers"] = {
 							{ "n", 19935 },	-- Soridormi
@@ -50,7 +60,16 @@ _.Zones =
 							}),
 						},
 					}),
-				})),
+					applyclassicphase(TBC_PHASE_ONE, q(10279, {	-- To The Master's Lair
+						["qg"] = 20142,	-- Steward of Time <Keepers of Time>
+						-- #if AFTER CATA
+						["coord"] = { 65.4, 49.6, TANARIS },
+						-- #else
+						["coord"] = { 66, 49.6, TANARIS },
+						-- #endif
+						["lvl"] = lvlsquish(66, 66, 15),
+					})),
+				}),
 			}),
 		}),
 	}),
