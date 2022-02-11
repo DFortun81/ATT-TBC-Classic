9ee0fdd064ccc8c0c414ae07a35fdb41f1082b5f
Crieve <dylanfortune@live.com>
2022-02-11 00:45:18 -0700

    Renamed Misc to Noblegarden in the holiday section.

--- .contrib/.db/05 - Holidays/Noblegarden.lua
+++ .contrib/.db/05 - Holidays/Noblegarden.lua
@@ -1,14 +1,18 @@
 --------------------------------------------
 --     H O L I D A Y S  M O D U L E       --
 --------------------------------------------
-_.Holidays =
-{
-	n(-383, {	-- Miscellaneous
+_.Holidays = { applyholiday(NOBLEGARDEN, {
+	-- #if ANYCLASSIC
+	["npcID"] = -51,
+	-- #else
+	["holidayID"] = 235477,
+	-- #endif
+	["groups"] = {
 		-- TODO: Easter Holiday
 		-- Brightly Colored Egg
 		i(6834),	-- Black Tuxedo
 		i(6835),	-- Black Tuxedo Pants
 		i(19028),	-- Elegant Dress
 		i(6833),	-- White Tuxedo Shirt
-	}),
-};
+	},
+})};
