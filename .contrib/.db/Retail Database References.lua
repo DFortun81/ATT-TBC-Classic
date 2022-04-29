dcab31a35eb6faea5d7b54d97b7637357c0f0e5c
Crieve <dylanfortune@live.com>
2022-04-28 21:52:42 -0700

    Searing Gorge now resides on the Retail Database!

--- .contrib/.db/Retail Database References.lua
+++ .contrib/.db/Retail Database References.lua
@@ -85,6 +85,7 @@
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Eversong Woods.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Ghostlands.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Isle of Quel'Danas.lua
+-- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Searing Gorge.lua
 root("Zones", m(EASTERN_KINGDOMS, {
 	["OnUpdate"] = [[function(t) table.sort(t.g, function (a, b) return string.lower(a.isRaid and (" " .. a.text) or a.text) < string.lower(b.isRaid and (" " .. b.text) or b.text) end)  end]],
 }));
