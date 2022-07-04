9e61e00e2a7a0f50a78884595fe6ecf2d220e485
Crieve <dylanfortune@live.com>
2022-07-04 13:44:34 -0700

    Western Plaguelands now resides on the Retail Database.

--- .contrib/.db/Retail Database References.lua
+++ .contrib/.db/Retail Database References.lua
@@ -101,6 +101,7 @@ root("Zones", m(KALIMDOR, {
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Swamp of Sorrows.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\The Hinterlands.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Tirisfal Glades.lua
+-- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Western Plaguelands.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Westfall.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Wetlands.lua
 root("Zones", m(EASTERN_KINGDOMS, {
