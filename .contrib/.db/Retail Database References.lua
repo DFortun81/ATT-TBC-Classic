36d8bcdbf3791230d5538859363342f802c58cbf
Crieve <dylanfortune@live.com>
2022-07-22 02:43:10 -0700

    Stranglethorn Vale now resides on the Retail DB.

--- .contrib/.db/Retail Database References.lua
+++ .contrib/.db/Retail Database References.lua
@@ -105,6 +105,7 @@ root("Zones", m(KALIMDOR, {
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Redridge Mountains.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Searing Gorge.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Silverpine Forest.lua
+-- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Stranglethorn Vale.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\Swamp of Sorrows.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\The Cape of Stranglethorn.lua
 -- #IMPORT: 02 - Outdoor Zones\\02 Eastern Kingdoms\\The Hinterlands.lua
